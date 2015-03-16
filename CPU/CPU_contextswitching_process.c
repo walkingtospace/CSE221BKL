6d31c67e5be0ae10deb55009acf2f94a01e140f9
walkingtospace@gmail.com <walkingtospace@gmail.com>
2015-03-16 10:11:25 +0000

    change context switching/memory bandwidth code

--- CPU/CPU_contextswitching_process.c
+++ CPU/CPU_contextswitching_process.c
@@ -1,25 +1,29 @@
 #include "cpu_test.h"
 #include <sched.h>
+#include "math.h"
 
-data_t cpu_cs_process(data_t overhead) {
+void cpu_cs_process(data_t overhead) {
 	unsigned start, end;
 	pid_t childPID;
-	int i, fd[2];
+	int i;
 	data_t total = 0;
+	int fd[2];
 	char buf;
+	float mean = 0, std = 0, min = 99999999, max = 0, median = 0;
+	float result[TASK_CREATION_NUM];	
 
 	if(pipe(fd)) {
 		printf("pipe() is failed\n");
 
-		return 0;
+		return ;
 	}
 
-	for(i = 0 ; i < TASK_CREATION_NUM ; ++i) {
+	for(i=0; i<TASK_CREATION_NUM ; ++i) {
 		childPID = fork();
 		
 		if(childPID < 0) {
 			printf("fork() was failed\n");
-		} else if(childPID > 0) { //parent
+		} else if(childPID > 0){ //parent
 			close(fd[0]);
 			write(fd[1], "t", 1); //write 1 byte		
 		
@@ -34,18 +38,54 @@ data_t cpu_cs_process(data_t overhead) {
 			}
 
 			close(fd[1]);
-			
 			while(read(fd[0], &buf, 1) > 0) { //wait until signal from the parent
 				//write(STDOUT_FILENO, &buf, 1);
 			}
 
+			//write(STDOUT_FILENO, "\n", 1);
+			//close(fd[0]);
+			
 			_exit(EXIT_SUCCESS);
 		}
 		
-		total += ((end - start)/2 - overhead);
+		float temp = ((end-start)/2-overhead);
+		if(temp < 0) printf("negative!");
+
+		total += temp;
+		result[i] = (float)temp;
+
+		if(min > temp) {
+			min = temp;
+		} 	
+
+		if(max < temp) {
+			max = temp;
+		}
+
+		total += (end - start - overhead);
+
 	}
 	
-	printf("\n");	
+	for(int i = 0; i < TASK_CREATION_NUM ; ++i) {
+		for(int j = i; j < TASK_CREATION_NUM ; ++j) {
+			if(result[i] > result[j]) {
+				float temp = result[i];
+				result[i] = result[j];
+				result[j] = temp;	
+			}
+		}
 
-	return (total/TASK_CREATION_NUM);
+		std += pow((result[i] - mean), 2);
+	}
+
+	median = result[TASK_CREATION_NUM/2];
+	mean = (total/TASK_CREATION_NUM);
+	std /= (TASK_CREATION_NUM);	
+	
+	printf("mean : %f \n", mean);
+	printf("median : %f \n", median);
+	printf("max : %f \n", max);
+	printf("min : %f \n", min);
+	printf("std : %f \n", sqrt(std));
 }
+
